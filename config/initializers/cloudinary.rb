964b3d015e97c9bab2c2c9e5da2f8e8434dbd201
Vishakh Aswani Kumar <vishakh@Vishakhs-MacBook-Pro.local>
2021-07-20 15:05:59 -0500

    feat: photo upload

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = "dz4ioqyl1"
+    config.api_key = "621394277598822"
+    config.api_secret = "ckw58hxxAN4wuJ1ps2Z4CxT8Sqg"
+    config.secure = "true"
+    config.cdn_subdomain = "true"
+end
